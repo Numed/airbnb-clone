0a4eeebe56d13ff219babdb8fed24dd95dfb7f32
Numed <ruschinets09@gmail.com>
2023-10-03 20:30:15 +0300

    basic architecture

---
+++ src/features/App/index.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+
+import Loader from "../../components/Loader";
+import Homepage from "../../pages/Home";
+
+const App = () => (
+  <Router basename="/e-commerce">
+              <Suspense fallback={<Loader />}>
+                <Routes>
+                  <Route path="/" element={<Homepage />} />
+                </Routes>
+              </Suspense>
+            </Router>
+);
+
+export default App;
---
+++ src/features/Contants/index.jsx
@@ -0,0 +1,127 @@
+import trip1 from "../../img/trips/trip-1.png";
+import trip2 from "../../img/trips/trip-2.png";
+import trip3 from "../../img/trips/trip-3.png";
+import trip4 from "../../img/trips/trip-4.png";
+import trip5 from "../../img/trips/trip-5.png";
+import trip6 from "../../img/trips/trip-6.png";
+import trip7 from "../../img/trips/trip-7.png";
+import trip8 from "../../img/trips/trip-8.png";
+import trip9 from "../../img/trips/trip-9.png";
+
+import banner1 from "../../img/banner/banner-flight.png";
+import banner2 from "../../img/banner/banner-hotels.png";
+
+import review1 from "../../img/reviews/review-1.png";
+import review2 from "../../img/reviews/review-2.png";
+import review3 from "../../img/reviews/review-3.png";
+
+export const tripCards = [
+  {
+    id: 1,
+    img: trip1,
+    title: "Istanbul, Turkey",
+    alt: "trip1",
+  },
+  {
+    id: 2,
+    img: trip2,
+    title: "Malé, Maldives",
+    alt: "trip2",
+  },
+  {
+    id: 3,
+    img: trip3,
+    title: "London, UK",
+    alt: "trip3",
+  },
+  {
+    id: 4,
+    img: trip4,
+    title: "Sydney, Australia",
+    alt: "trip4",
+  },
+  {
+    id: 5,
+    img: trip5,
+    title: "Paris, France",
+    alt: "trip5",
+  },
+  {
+    id: 6,
+    img: trip6,
+    title: "Tokyo, Japan",
+    alt: "trip6",
+  },
+  {
+    id: 7,
+    img: trip7,
+    title: "Baku, Azerbaijan",
+    alt: "trip7",
+  },
+  {
+    id: 8,
+    img: trip8,
+    title: "New York, US",
+    alt: "trip8",
+  },
+  {
+    id: 9,
+    img: trip9,
+    title: "Dubai, UAE",
+    alt: "trip9",
+  },
+];
+
+export const bannerCards = [
+  {
+    id: 1,
+    title: "Flights",
+    description:
+      "Search Flights & Places Hire to our most popular destinations",
+    img: banner1,
+    alt: "Flights image",
+  },
+  {
+    id: 2,
+    title: "Hotels",
+    description: "Search hotels & Places Hire to our most popular destinations",
+    img: banner2,
+    alt: "Hotels image",
+  },
+];
+
+export const reviewsCards = [
+  {
+    id: 1,
+    title: "A real sense of community, nurtured",
+    description:
+      "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.",
+    img: review1,
+    alt: "Review card",
+    rating: 5,
+    author: "Olga",
+    position: "Weave Studios – Kai Tak",
+  },
+  {
+    id: 2,
+    title: "The facilities are superb. Clean, slick, bright",
+    description:
+      "A real sense of community, nurtured”Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.View moreOlgaWeave Studios – Kai TakGoogle",
+    img: review2,
+    alt: "Review card",
+    rating: 5,
+    author: "Thomas",
+    position: "Weave Studios – Kai Tak",
+  },
+  {
+    id: 3,
+    title: "A real sense of community, nurtured",
+    description:
+      "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.",
+    img: review3,
+    alt: "Review card",
+    rating: 5,
+    author: "Eliot",
+    position: "Weave Studios – Kai Tak",
+  },
+];
