b2528fb910cdf27a09b1bfc8feca708988567ae6
Numed <ruschinets09@gmail.com>
2023-11-09 18:00:52 +0200

    add sign in section to details and history section

--- src/features/Details/index.jsx
+++ src/features/Details/index.jsx
@@ -9,10 +9,14 @@ import { ImLocation2 } from "react-icons/im";
 
 import flightPartner from "../../img/searchFlights/flight1.png";
 import { cn } from "../../utils";
+import { useActiveUser } from "../../store";
+import { Link } from "react-router-dom";
 
 const DetailsContent = ({ detail }) => {
   const [isFlight, setIsFlight] = useState(true);
   const [selectedPayment, setSelectedPayment] = useState("full-price");
+  const { user } = useActiveUser();
+
   return (
     <main className="flex items-center justify-center flex-col w-full h-full px-[5rem] mt-12">
       <div className="flex items-center justify-between">
@@ -193,6 +197,34 @@ const DetailsContent = ({ detail }) => {
                 />
               </label>
             </div>
+          </section>
+          <section className="w-full h-full">
+            {user === undefined ? (
+              <div className="flex flex-col items-start justify-center bg-white rounded-md p-4">
+                <h3 className="text-2xl text-blackishGreen font-semibold">
+                  You are not authorized
+                </h3>
+                <Link
+                  className="mt-3 bg-mintGreen/90 hover:bg-mintGreen w-full h-full px-3 py-4 text-center text-white transition-all"
+                  to="/sign-in"
+                >
+                  Go to Login
+                </Link>
+                <h4 className="text-blackishGreen/60 text-lg mt-2 mb-0 mx-auto">
+                  Not register yet?
+                  <Link
+                    className="text-blue-300 font-medium text-base ml-3"
+                    to="/sign-up"
+                  >
+                    Register now
+                  </Link>
+                </h4>
+              </div>
+            ) : (
+              <button className="text-white text-xl bg-mintGreen hover:bg-mintGreen/70 text-center w-full py-4 transition-colors">
+                Book now
+              </button>
+            )}
           </section>
         </div>
         <aside className="ml-10 bg-white rounded-xl p-6">
--- src/features/Profile/index.jsx
+++ src/features/Profile/index.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import { IoPencil } from "react-icons/io5";
+import { IoBed, IoPencil } from "react-icons/io5";
 import { RiEditBoxFill } from "react-icons/ri";
 
 import ava from "../../img/profile/ava.png";
 import { cn } from "../../utils";
+import { useActiveUser } from "../../store";
+import { MdFlight } from "react-icons/md";
 
 const ProfileContainer = () => {
   const [activeSection, setActiveSection] = useState("Account");
+  const [historySection, setHistorySection] = useState("Account");
+  const { user } = useActiveUser();
   return (
     <section className="p-[6.5rem]">
       <div className="w-full h-[22rem] bg-profileBackground bg-center bg-no-repeat bg-cover rounded-xl"></div>
@@ -48,76 +52,118 @@ const ProfileContainer = () => {
           </button>
         </div>
         <div className="mt-10 w-full h-auto">
-          <h2 className="mb-4 font-bold text-3xl">Account</h2>
-          <div className="bg-white shadow-lg rounded-2xl py-8 px-6 w-full space-y-8">
-            <div className="flex items-center justify-between">
-              <div>
-                <h4 className="mb-2 text-blackishGreen/75">Name</h4>
-                <h3 className="text-xl text-blackishGreen font-semibold">
-                  John Doe
-                </h3>
+          {activeSection === "Account" ? (
+            <>
+              <h2 className="mb-4 font-bold text-3xl">Account</h2>
+              <div className="bg-white shadow-lg rounded-2xl py-8 px-6 w-full space-y-8">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h4 className="mb-2 text-blackishGreen/75">Name</h4>
+                    <h3 className="text-xl text-blackishGreen font-semibold">
+                      John Doe
+                    </h3>
+                  </div>
+                  <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
+                    <RiEditBoxFill className="mr-1" /> Change
+                  </button>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h4 className="mb-2 text-blackishGreen/75">Email</h4>
+                    <h3 className="text-xl text-blackishGreen font-semibold">
+                      john.doe@gmail.com
+                    </h3>
+                  </div>
+                  <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
+                    <RiEditBoxFill className="mr-1" />
+                    Change
+                  </button>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h4 className="mb-2 text-blackishGreen/75">Password</h4>
+                    <h3 className="text-xl text-blackishGreen font-semibold">
+                      ********
+                    </h3>
+                  </div>
+                  <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
+                    <RiEditBoxFill className="mr-1" /> Change
+                  </button>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h4 className="mb-2 text-blackishGreen/75">Phone number</h4>
+                    <h3 className="text-xl text-blackishGreen font-semibold">
+                      +1 000-000-0000
+                    </h3>
+                  </div>
+                  <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
+                    <RiEditBoxFill className="mr-1" /> Change
+                  </button>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h4 className="mb-2 text-blackishGreen/75">Address</h4>
+                    <h3 className="text-xl text-blackishGreen font-semibold">
+                      St 32 main downtown, Los Angeles, California, USA
+                    </h3>
+                  </div>
+                  <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
+                    <RiEditBoxFill className="mr-1" /> Change
+                  </button>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h4 className="mb-2 text-blackishGreen/75">
+                      Date of birth
+                    </h4>
+                    <h3 className="text-xl text-blackishGreen font-semibold">
+                      01-01-1992
+                    </h3>
+                  </div>
+                  <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
+                    <RiEditBoxFill className="mr-1" /> Change
+                  </button>
+                </div>
               </div>
-              <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
-                <RiEditBoxFill className="mr-1" /> Change
-              </button>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <h4 className="mb-2 text-blackishGreen/75">Email</h4>
-                <h3 className="text-xl text-blackishGreen font-semibold">
-                  john.doe@gmail.com
+            </>
+          ) : (
+            <div>
+              {user ? (
+                <>
+                  <h4 className="text-4xl text-black font-bold">History</h4>
+                  <div className="mt-8 w-full h-auto flex items-center justify-center">
+                    <button
+                      className={cn(
+                        historySection === "Flights" &&
+                          "underline decoration-4 underline-offset-8 decoration-mintGreen",
+                        "w-1/2 text-base font-semibold text-blackishGreen bg-white rounded-s-xl px-6 py-8 flex items-center justify-center"
+                      )}
+                      onClick={() => setHistorySection("Flights")}
+                    >
+                      <MdFlight size="1.5em" className="mr-1" />
+                      Flights
+                    </button>
+                    <button
+                      className={cn(
+                        historySection === "Stays" &&
+                          "underline decoration-4 underline-offset-8 decoration-mintGreen",
+                        "w-1/2 border-l border-gray-300 text-base font-semibold text-blackishGreen bg-white rounded-e-xl px-6 py-8 flex items-center justify-center"
+                      )}
+                      onClick={() => setHistorySection("Stays")}
+                    >
+                      <IoBed size="1.5em" className="mr-1" />
+                      Stays
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <h3 className="text-2xl text-gray-400 font-bold text-center w-full">
+                  You don't have history yet
                 </h3>
-              </div>
-              <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
-                <RiEditBoxFill className="mr-1" />
-                Change
-              </button>
+              )}
             </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <h4 className="mb-2 text-blackishGreen/75">Password</h4>
-                <h3 className="text-xl text-blackishGreen font-semibold">
-                  ********
-                </h3>
-              </div>
-              <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
-                <RiEditBoxFill className="mr-1" /> Change
-              </button>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <h4 className="mb-2 text-blackishGreen/75">Phone number</h4>
-                <h3 className="text-xl text-blackishGreen font-semibold">
-                  +1 000-000-0000
-                </h3>
-              </div>
-              <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
-                <RiEditBoxFill className="mr-1" /> Change
-              </button>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <h4 className="mb-2 text-blackishGreen/75">Address</h4>
-                <h3 className="text-xl text-blackishGreen font-semibold">
-                  St 32 main downtown, Los Angeles, California, USA
-                </h3>
-              </div>
-              <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
-                <RiEditBoxFill className="mr-1" /> Change
-              </button>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <h4 className="mb-2 text-blackishGreen/75">Date of birth</h4>
-                <h3 className="text-xl text-blackishGreen font-semibold">
-                  01-01-1992
-                </h3>
-              </div>
-              <button className="flex items-center justify-center p-4 border border-mintGreen hover:bg-mintGreen hover:text-white transition-all">
-                <RiEditBoxFill className="mr-1" /> Change
-              </button>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </section>
